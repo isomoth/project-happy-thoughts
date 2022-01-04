baa98b15b9b0d1af272c3247727b165e6c71fffd
isomoth <ic.gonzalez35@hotmail.com>
2022-01-04 21:29:03 +0100

    added link to my own API

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,3 +1,4 @@
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts';
+export const API_URL =
+  'https://happy-thoughts-api-isabel.herokuapp.com/thoughts';
 export const LIKES_URL = (thoughtId) =>
-  `https://happy-thoughts-technigo.herokuapp.com/thoughts/${thoughtId}/like`;
+  `https://happy-thoughts-api-isabel.herokuapp.com/thoughts/${thoughtId}/like`;
